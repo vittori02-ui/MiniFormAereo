b54cd9a6b07632e243b56f8b82c709261b0dc8a7
vittori02-ui <vittorioprivitera02@gmail.com>
2026-05-25 13:00:42 +0200

    totale and other check

--- esPreVerifica.js
+++ esPreVerifica.js
@@ -18,4 +18,39 @@ function scelta() {
         alert("Scegli una meta!!!");
         return;
     }
+    numBig=parseInt(numBig.value);
+    if(numBig<=0) numBig=1;
+    let extraTot=0;
+    let extraText="";
+    if(bag.checked) 
+    {
+        extraTot+=parseInt(bag.value);
+        extraText+="Bagaglio extra 15€\n";
+    }
+    if(baby.checked) 
+    {
+        extraTot+=parseInt(baby.value);
+        extraText+="Posto bambino 20€\n";
+    }
+    if(!bag.checked&&!baby.checked) 
+    {
+        extraText="NIENTE\n";
+    }
+    //let nuovaDiv=document.createElement("div");
+    //box.style.width="200px";
+    //box.style.height="50px";
+    box.append("RIEPILOGO ORDINE"+"\n");
+    box.append("Nome: "+nome+"\n");
+    box.append("Cognome: "+cognome+"\n");
+    box.append("Meta: "+metaNome+"\n");
+    box.append("Numero biglietti: "+numBig+"\n");
+    box.append("Data di andata: "+andata+"\n");
+    box.append("Data di ritorno: "+rit+"\n");
+    box.append("Servizi extra: "+"\n"+extraText+"\n");
+    box.append("TOTALE: "+totale(extraTot)+"€");
+}
+function totale(extraTot)
+{   
+    let metaPrez=parseInt(meta.value);  
+    return metaPrez*numBig+extraTot;
 }
